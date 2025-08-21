f61f28758934bc3299135b8b2b1159736a85c084
Arran4567 <Arran4567@gmail.com>
2025-08-21 23:43:31 +0100

    Removed aws region

--- scripts/web/set-env.sh
+++ scripts/web/set-env.sh
@@ -6,7 +6,7 @@ export ENVIRONMENT_NAME=$1
 export PROJECT_NAME="techtest"
 
 export TERRAGRUNT_DIRECTORY="deploy/regional/web"
-export PLAN_FILE_PATH="/tmp/${AWS_DEFAULT_REGION}-${PROJECT_NAME}.tfplan"
+export PLAN_FILE_PATH="/tmp/${PROJECT_NAME}.tfplan"
 export ARTIFACTS_DIR="${PWD}/build"
 export TF_VAR_environment_name=${ENVIRONMENT_NAME}
 export TF_VAR_environment=$1
